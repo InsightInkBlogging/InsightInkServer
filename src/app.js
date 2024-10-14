e43f15242b320586317777a8f0a8df3b2a6d98fe
das-codes03 <arghadeep.d.das@gmail.com>
2024-10-14 13:06:46 +0530

    Updated app.js

--- src/app.js
+++ src/app.js
@@ -10,40 +10,40 @@ const api = require("./api");
 
 const app = express();
 
-const bufferToJSONMiddleware = (req, res, next) => {
-  if (req.body && req.body instanceof Buffer) {
-    try {
-      req.body = JSON.parse(req.body.toString());
-    } catch (err) {
-      // return res
-      //   .status(400)
-      //   .json({
-      //     error: "Invalid JSON data",
-      //     data: JSON.stringify(err),
-      //     payload: JSON.stringify(req.body),
-      //   });
-    }
-  }
+// const bufferToJSONMiddleware = (req, res, next) => {
+//   if (req.body && req.body instanceof Buffer) {
+//     try {
+//       req.body = JSON.parse(req.body.toString());
+//     } catch (err) {
+//       // return res
+//       //   .status(400)
+//       //   .json({
+//       //     error: "Invalid JSON data",
+//       //     data: JSON.stringify(err),
+//       //     payload: JSON.stringify(req.body),
+//       //   });
+//     }
+//   }
 
-  next();
-};
+//   next();
+// };
 
 app.use(morgan("dev"));
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-app.use(bufferToJSONMiddleware);
+// app.use(bufferToJSONMiddleware);
 
 const connection = null;
 
 let invocationNumber = 1;
 app.get("/", (req, res) => {
-  invocationNumber++;
-  return res.json({
-    message: "🦄🌈✨👋🌎🌍🌏✨🌈🦄",
-    invocationNumber,
-  });
+	invocationNumber++;
+	return res.json({
+		message: "🦄🌈✨👋🌎🌍🌏✨🌈🦄",
+		invocationNumber,
+	});
 });
 
 app.use("/api", api);
